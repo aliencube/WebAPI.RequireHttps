ce9eb0049152e99f49422e1cd3fdd56e0ed747d1
Justin Yoo <justin.yoojh@gmail.com>
2014-07-13 01:02:48 +1000

    Added WebApi.RequireHttps project.

---
+++ SourceCodes/WebApi.RequireHttps/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Aliencube.WebApi.RequireHttps")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("50298330-ed2a-4ec3-bdd0-9b700031a10b")]
