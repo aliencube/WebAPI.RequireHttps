fb4126aecbe76e8a8d0d08a8755cec408d56c578
Justin Yoo <justin.yoo@aliencube.com>
2014-07-15 13:21:19 +1000

    Added test project and NuGet libraries for test.

---
+++ SourceCodes/WebApi.RequireHttps.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Aliencube.WebApi.RequireHttps.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c5e760d3-9087-424f-b876-95b725b2ae8c")]
