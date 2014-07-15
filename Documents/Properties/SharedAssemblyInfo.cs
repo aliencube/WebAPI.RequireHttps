9e0865029721ca1bbd26c149245944a00f720150
Justin Yoo <justin.yoo@aliencube.com>
2014-07-16 00:54:16 +1000

    Versioned up to 1.5.0.0

--- Documents/Properties/SharedAssemblyInfo.cs
+++ Documents/Properties/SharedAssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyCompany("Aliencube Community")]
-[assembly: AssemblyProduct("WebApi.RequireHttps")]
+[assembly: AssemblyProduct("Aliencube.WebApi.RequireHttps")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("Aliencube")]
 [assembly: AssemblyCulture("")]
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.*")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.5.0.*")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
