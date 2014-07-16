a7cb91a5df94129b92d791123828fe4df668a922
Justin Yoo <justin.yoo@aliencube.com>
2014-07-16 14:12:06 +1000

    Updated assembly information.

--- Documents/Properties/SharedAssemblyInfo.cs
+++ Documents/Properties/SharedAssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyCompany("Aliencube Community")]
-[assembly: AssemblyProduct("Aliencube.WebApi.RequireHttps")]
+[assembly: AssemblyProduct("WebApi.RequireHttps")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("Aliencube")]
 [assembly: AssemblyCulture("")]
